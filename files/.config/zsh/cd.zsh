a1d53af320f16c9754bab0561ee69b9a68bf7563
Rylan Polster <rslpolster@gmail.com>
2024-05-29 12:19:10 -0400

    Use home env var in cd aliases

--- files/.config/zsh/cd.zsh
+++ files/.config/zsh/cd.zsh
@@ -1,15 +1,15 @@
 alias cdgr='cd "$(git root)"'
-alias cddf='cd "$HOME/.dotfiles/"'
-alias des='cd "$HOME/Desktop/"'
-alias dev='cd "$HOME/Development/"'
-alias doc='cd "$HOME/Documents/"'
-alias dow='cd "$HOME/Downloads/"'
-alias hom='cd "$HOME/Development/homebrew/"'
-alias brepo="cd $HOMEBREW_PREFIX"
-alias bcore="cd $HOMEBREW_PREFIX/Library/Taps/homebrew/homebrew-core/"
-alias bcask="cd $HOMEBREW_PREFIX/Library/Taps/homebrew/homebrew-cask/"
-alias cellar="cd $HOMEBREW_PREFIX/Cellar/"
-alias caskroom="cd $HOMEBREW_PREFIX/Caskroom/"
+alias cddf="cd '$HOME/.dotfiles/'"
+alias des="cd '$HOME/Desktop/'"
+alias dev="cd '$HOME/Development/'"
+alias doc="cd '$HOME/Documents/'"
+alias dow="cd '$HOME/Downloads/'"
+alias hom="cd '$HOME/Development/homebrew/'"
+alias brepo="cd '$HOMEBREW_PREFIX'"
+alias bcore="cd '$HOMEBREW_PREFIX/Library/Taps/homebrew/homebrew-core/'"
+alias bcask="cd '$HOMEBREW_PREFIX/Library/Taps/homebrew/homebrew-cask/'"
+alias cellar="cd '$HOMEBREW_PREFIX/Cellar/'"
+alias caskroom="cd '$HOMEBREW_PREFIX/Caskroom/'"
 cdb() {
     cd "$(brew --repository $1)"
 }
