f4ff2f34755ff511d3947d456d69db229492a43f
Rylan Polster <rslpolster@gmail.com>
2024-05-29 12:11:37 -0400

    Use homebrew env var in cd aliases

--- files/.config/zsh/cd.zsh
+++ files/.config/zsh/cd.zsh
@@ -5,12 +5,12 @@ alias dev='cd "$HOME/Development/"'
 alias doc='cd "$HOME/Documents/"'
 alias dow='cd "$HOME/Downloads/"'
 alias hom='cd "$HOME/Development/homebrew/"'
-alias brepo='cd /opt/homebrew'
-alias bcore='cd /opt/homebrew/Library/Taps/homebrew/homebrew-core/'
-alias bcask='cd /opt/homebrew/Library/Taps/homebrew/homebrew-cask/'
-alias cellar='cd /opt/homebrew/Cellar/'
-alias caskroom='cd /opt/homebrew/Caskroom/'
+alias brepo="cd $HOMEBREW_PREFIX"
+alias bcore="cd $HOMEBREW_PREFIX/Library/Taps/homebrew/homebrew-core/"
+alias bcask="cd $HOMEBREW_PREFIX/Library/Taps/homebrew/homebrew-cask/"
+alias cellar="cd $HOMEBREW_PREFIX/Cellar/"
+alias caskroom="cd $HOMEBREW_PREFIX/Caskroom/"
 cdb() {
     cd "$(brew --repository $1)"
 }
-alias cdbl='cd $(brew --repository)/Library/Homebrew'
+alias cdbl="cd $HOMEBREW_PREFIX/Library/Homebrew"
