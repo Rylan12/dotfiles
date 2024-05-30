3b852ef837340be6ceb4bb7dfe377fc0926ddb66
Rylan Polster <rslpolster@gmail.com>
2024-05-30 18:12:25 -0400

    Rearrange plugins

--- files/.config/zsh/plugins.zsh
+++ files/.config/zsh/plugins.zsh
@@ -1,8 +1,3 @@
-# Auto-suggestions
-source "$HOMEBREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
-export ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
-export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
-
 # Vi mode
 source "$HOMEBREW_PREFIX/opt/zsh-vi-mode/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh"
 
@@ -23,5 +18,10 @@ zle -N self-insert url-quote-magic
 autoload -Uz git-escape-magic
 git-escape-magic
 
-# Syntax highlighting (this should be sourced last)
+# Syntax highlighting (this should be sourced last, except for zsh-autosuggestions)
 source "$HOMEBREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+
+# Auto-suggestions (this should be sourced last)
+source "$HOMEBREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+export ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
+export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
