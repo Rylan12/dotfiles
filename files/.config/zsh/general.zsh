c72708a8d677c9cfbb7a8c4b56a04d3f1942615f
Rylan Polster <rslpolster@gmail.com>
2024-05-30 01:33:59 -0400

    Config additions to homebrew and general

--- files/.config/zsh/general.zsh
+++ files/.config/zsh/general.zsh
@@ -1,8 +1,15 @@
+# Use extended globbing
+setopt extended_glob
+
 # Add custom executables to PATH
 export PATH="$HOME/bin:$PATH"
 
 # Use nvim regardless
 alias vim='nvim'
+
+# Highlight the matching text in grep
+alias grep='grep --color=auto'
+alias sgrep='grep -R -n -H -C 5 --exclude-dir={.git,.svn,CVS}'
 
 # reload current directory
 alias reload='[[ ! -d "$(pwd)" ]] && echo "$(pwd) no longer exists" && return 1 || cd "$(pwd)"'
@@ -19,6 +26,20 @@ alias cx='chmod +x'
 # Aliases to protect against overwriting
 alias cp='cp -i'
 alias mv='mv -i'
+alias rm='rm -i'
+
+# Global aliases
+alias -g H='| head'
+alias -g T='| tail'
+alias -g G='| grep'
+alias -g L="| less"
+alias -g B="| bat"
+alias -g LL="2>&1 | less"
+alias -g CA="2>&1 | cat -A"
+alias -g NE="2> /dev/null"
+alias -g NUL="> /dev/null 2>&1"
+alias -g P="| pbcopy"
+alias -g HOME="$HOME"
 
 # Download youtube video as wav file
 alias ytwav='yt-dlp --extract-audio --audio-format wav'
@@ -28,3 +49,6 @@ alias path='echo "$PATH" | tr ":" "\n"'
 
 # Edit secrets file
 alias secrets="nvim $ZSHDIR/secrets.zsh"
+
+# Reload configuration
+alias src="source $HOME/.zshrc"
--- files/.config/zsh/homebrew.zsh
+++ files/.config/zsh/homebrew.zsh
@@ -4,6 +4,7 @@ export HOMEBREW_DEVELOPER=1
 export HOMEBREW_FORCE_BREWED_GIT=1
 export HOMEBREW_GITHUB_PACKAGES_USER="Rylan12"
 export HOMEBREW_NO_AUTO_UPDATE=1
+export HOMEBREW_NO_ENV_HINTS=1
 export HOMEBREW_PRY=1
 eval "$(/opt/homebrew/bin/brew shellenv)"
 FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
