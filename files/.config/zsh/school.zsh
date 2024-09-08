9915ac63fa22961e04d30b21b459ec88dd7bbc94
Rylan Polster <rslpolster@gmail.com>
2024-09-08 13:08:28 -0400

    Update school workspaces and aliases

--- files/.config/zsh/school.zsh
+++ files/.config/zsh/school.zsh
@@ -1,2 +1,5 @@
 # OCaml init (for CPSC 421)
 [[ ! -r '/Users/rylanpolster/.opam/opam-init/init.zsh' ]] || source '/Users/rylanpolster/.opam/opam-init/init.zsh' > /dev/null 2> /dev/null
+
+# Alias for opening a VNC session for EE 426
+alias vnc426="open vnc://:userpass@localhost:8500"
