bb82cefeae5b82c6efa791f2113aeba69eaa4bfc
Rylan Polster <rslpolster@gmail.com>
2024-09-08 13:09:54 -0400

    Update `nvim` ruby version and disable `perl`

--- files/.config/nvim/lua/config/options.lua
+++ files/.config/nvim/lua/config/options.lua
@@ -4,6 +4,6 @@
 
 -- Language provider setup
 vim.g.python3_host_prog = "~/.pyenv/versions/neovim/bin/python"
-vim.g.ruby_host_prog = "~/.rbenv/versions/3.2.2/bin/neovim-ruby-host"
+vim.g.ruby_host_prog = "~/.rbenv/versions/3.3.4/bin/neovim-ruby-host"
 vim.g.node_host_prog = "~/.nodenv/versions/20.13.1/bin/neovim-node-host"
-vim.g.perl_host_prog = "/opt/homebrew/bin/perl"
+vim.g.loaded_perl_provider = 0
