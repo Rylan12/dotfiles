1a6a95539a0e0f21c350450843fdc96f148c7568
Rylan Polster <rslpolster@gmail.com>
2024-09-15 22:36:08 -0400

    Add school cd aliases

--- files/.config/zsh/school.zsh
+++ files/.config/zsh/school.zsh
@@ -3,3 +3,10 @@
 
 # Alias for opening a VNC session for EE 426
 alias vnc426="open vnc://:userpass@localhost:8500"
+
+# CD Aliases for school directories
+alias game-engines="cd ${HOME}/School/cs411/monorepo-Rylan12"
+alias security="cd ${HOME}/School/cs413"
+alias compilers="cd ${HOME}/School/cs421/current"
+alias graphics="cd ${HOME}/School/cs478"
+alias silicon="cd ${HOME}/School/ee426"
