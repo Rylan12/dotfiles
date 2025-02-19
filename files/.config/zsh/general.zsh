69c3936ddcfe092270d59baa2b57a9ff20db9064
Rylan Polster <rslpolster@gmail.com>
2025-02-18 20:25:03 -0500

    Add jq global alias

--- files/.config/zsh/general.zsh
+++ files/.config/zsh/general.zsh
@@ -39,6 +39,7 @@ alias -g T='| tail'
 alias -g G='| grep'
 alias -g L="| less"
 alias -g B="| bat"
+alias -g J="| jq -C | less"
 alias -g LL="2>&1 | less"
 alias -g CA="2>&1 | cat -A"
 alias -g NE="2> /dev/null"
