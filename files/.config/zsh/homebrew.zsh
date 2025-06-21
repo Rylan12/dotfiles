96ed5491679dea3aaf67f64cc1653d9c0db0d896
Rylan Polster <rslpolster@gmail.com>
2025-06-21 17:43:07 -0400

    Have homebrew find its own installation better

--- files/.config/zsh/homebrew.zsh
+++ files/.config/zsh/homebrew.zsh
@@ -1,3 +1,23 @@
+# Check if Homebrew is installed by checking the following locations:
+# 1. The `brew` command is available in the PATH
+# 2. The HOMEBREW_PREFIX environment variable is set and points to a valid Homebrew installation
+# 3. The default Homebrew installation paths for macOS and Linux
+if command_exists brew; then
+  HOMEBREW_EXECUTABLE="$(command -v brew)"
+elif [[ -n "${HOMEBREW_PREFIX-}" && -f "${HOMEBREW_PREFIX}/bin/brew" ]]; then
+  HOMEBREW_EXECUTABLE="${HOMEBREW_PREFIX}/bin/brew"
+elif [[ -f /opt/homebrew/bin/brew ]]; then
+  HOMEBREW_EXECUTABLE="/opt/homebrew/bin/brew"
+elif [[ -f /home/linuxbrew/.linuxbrew/bin/brew ]]; then
+  HOMEBREW_EXECUTABLE="/home/linuxbrew/.linuxbrew/bin/brew"
+fi
+
+# If there's no Homebrew executable found, exit
+if [[ -n "${HOMEBREW_EXECUTABLE-}" ]]; then
+  printf 'Warning: Homebrew was not found. This will likely cause issues with your shell setup.\n'
+  return 1
+fi
+
 export HOMEBREW_BAT=1
 export HOMEBREW_BOOTSNAP=1
 export HOMEBREW_DEVELOPER=1
@@ -5,7 +25,7 @@ export HOMEBREW_FORCE_BREWED_GIT=1
 export HOMEBREW_GITHUB_PACKAGES_USER="Rylan12"
 export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_NO_ENV_HINTS=1
-eval "$(/opt/homebrew/bin/brew shellenv)"
+eval "$($HOMEBREW_EXECUTABLE shellenv)"
 FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
 
 alias -g APIOFF='HOMEBREW_NO_INSTALL_FROM_API=1'
