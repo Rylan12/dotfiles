a92a09c81516365d044c0b4efe11f5fe3e2984b0
Rylan Polster <rslpolster@gmail.com>
2025-08-06 09:16:41 -0400

    Update node version for neovim

--- files/.config/nvim/lua/config/options.lua
+++ files/.config/nvim/lua/config/options.lua
@@ -5,5 +5,5 @@
 -- Language provider setup
 vim.g.python3_host_prog = "~/.pyenv/versions/neovim/bin/python"
 vim.g.ruby_host_prog = "~/.rbenv/versions/3.3.4/bin/neovim-ruby-host"
-vim.g.node_host_prog = "~/.nodenv/versions/20.13.1/bin/neovim-node-host"
+vim.g.node_host_prog = "~/.nodenv/versions/22.16.0/bin/neovim-node-host"
 vim.g.loaded_perl_provider = 0
