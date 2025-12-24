d5eacfbab445e2ebb4db42bdcf95ca2bd5aa49ca
Rylan Polster <rslpolster@gmail.com>
2025-12-24 12:56:52 -0500

    Remove python from neovim setup

--- files/.config/nvim/lua/config/options.lua
+++ files/.config/nvim/lua/config/options.lua
@@ -3,7 +3,6 @@
 -- Add any additional options here
 
 -- Language provider setup
-vim.g.python3_host_prog = "~/.pyenv/versions/neovim/bin/python"
 vim.g.ruby_host_prog = "~/.rbenv/versions/3.3.4/bin/neovim-ruby-host"
 vim.g.node_host_prog = "~/.nodenv/versions/22.16.0/bin/neovim-node-host"
 vim.g.loaded_perl_provider = 0
