80ba0095c0b00f532d732b1440551e8947f8653a
Rylan Polster <rslpolster@gmail.com>
2025-12-24 12:56:09 -0500

    Remove some zsh plugins

--- files/.config/zsh/plugins.zsh
+++ files/.config/zsh/plugins.zsh
@@ -1,23 +1,6 @@
 # Vi mode
 source "$HOMEBREW_PREFIX/opt/zsh-vi-mode/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh"
 
-# Auto-pair
-source "$HOMEBREW_PREFIX/share/zsh-autopair/autopair.zsh"
-
-# git-extras completions
-source "$HOMEBREW_PREFIX/opt/git-extras/share/git-extras/git-extras-completion.zsh"
-
-# zoxide (z command)
-_evalcache zoxide init zsh
-
-# url-quote-magic
-autoload -Uz url-quote-magic
-zle -N self-insert url-quote-magic
-
-# git-escpe-magic
-autoload -Uz git-escape-magic
-git-escape-magic
-
 # Syntax highlighting (this should be sourced last, except for zsh-autosuggestions)
 source "$HOMEBREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 
