67f761d8b8b2489906692dcb97d54ec92ff5ea7a
Rylan Polster <rylan.polster@shopify.com>
2026-01-05 17:29:31 -0500

    Remove nvm configuration

--- files/.config/zsh/nvm.zsh
+++
@@ -1,6 +0,0 @@
-# Return if nvm is not installed
-command_exists nvm || return
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
