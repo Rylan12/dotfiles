785f0ecc1c36c9d54e4b8dd9f543512af812b223
Rylan Polster <rylan.polster@shopify.com>
2026-01-12 14:40:26 -0500

    Use mise installation for neovim

--- files/.config/nvim/lua/config/options.lua
+++ files/.config/nvim/lua/config/options.lua
@@ -2,7 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Prepend mise shims to PATH
+vim.env.PATH = vim.env.HOME .. "/.local/share/mise/shims:" .. vim.env.PATH
+
 -- Language provider setup
-vim.g.ruby_host_prog = "~/.rbenv/versions/3.3.4/bin/neovim-ruby-host"
-vim.g.node_host_prog = "~/.nodenv/versions/22.16.0/bin/neovim-node-host"
-vim.g.loaded_perl_provider = 0
+vim.g.python3_host_prog = "~/.local/share/mise/installs/python/latest/bin/python3"
+vim.g.ruby_host_prog = "~/.local/share/mise/installs/ruby/latest/bin/neovim-ruby-host"
+vim.g.node_host_prog = "~/.local/share/mise/installs/node/latest/bin/neovim-node-host"
