fa6fb0328f92a9edd42615df554ed1b4fa64f26c
Rylan Polster <rslpolster@gmail.com>
2026-03-09 06:13:30 -0400

    Replace `pure` with `rslprompt`

--- files/.config/zsh/appearance.zsh
+++ files/.config/zsh/appearance.zsh
@@ -1,10 +1,3 @@
 export CLICOLOR=1
 
-# Pure prompt
-autoload -U promptinit; promptinit
-zstyle ':prompt:pure:prompt:success' color green
-zstyle :prompt:pure:git:stash show yes
-prompt pure
-
-# Disable Pure's title-setting so we can manage it ourselves
-prompt_pure_set_title() {}
+eval "$(rslprompt init zsh)"
